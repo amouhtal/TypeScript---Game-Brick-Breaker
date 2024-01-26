ac9e357912aadfe1ce2022f3643cce526d63634c
abderrahmane mouhtal <mouhtal.abderrahmane@gmail.com>
2024-01-26 16:00:03 +0100

    scaffold

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,15 @@
-// Start here
-console.log("Start here!");
+import { createBricks } from "./helpers";
+import { CanvasView } from "./view/CanvasView";
+
+function startGame(view: CanvasView) {
+    // reset display
+    score = 0;
+
+    view.drawInfo("");
+    view.drawScore(0);
+    // create all bricks
+
+    const bricks = createBricks();
+    gameLoop(view, bricks);
+    
+}
