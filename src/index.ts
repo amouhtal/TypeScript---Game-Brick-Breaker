227774448791c148de690aea8bfb51106b9a26ab
abderrahmane mouhtal <mouhtal.abderrahmane@gmail.com>
2024-01-26 16:11:11 +0100

    scaffold

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,54 @@
+import { CanvasView } from "./view/CanvasView";
+import { Ball } from "./sprites/Ball";
+import { Brick } from "./sprites/Brick";
+import { Paddle } from "./sprites/Paddle";
+
+import PADDLE_IMAGE from "./images/paddle.png";
+import BALL_IMAGE from "./images/ball.png";
+
+import {
+    PADDLE_SPEED,
+    PADDLE_WIDTH,
+    PADDLE_HEIGHT,
+    PADDLE_STARTX,
+    BALL_SPEED,
+    BALL_SIZE,
+    BALL_STARTX,
+    BALL_STARTY,
+} from "./setup";
+
+
+let gameOver = false;
+let score = 0;
+
+function setGameOver(view: CanvasView){
+    view.drawInfo('Game Ovet!');
+    gameOver = false;
+}
+
+function setGameWin(view: CanvasView){
+    view.drawInfo('Game Won!');
+    gameOver = false;
+}
+
+function gameLoop(
+    view: CanvasView,
+    bricks: Brick[],
+    paddle: Paddle,
+    ball: Ball
+){
+
+}
+
+function startGame(view: CanvasView){
+
+}
+
+// create a new view
+
+const view = new CanvasView('#playField');
+view.initStartButton(startGame);
+
 import { createBricks } from "./helpers";
 import { CanvasView } from "./view/CanvasView";
 
